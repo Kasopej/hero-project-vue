27a164440537713323df49077449421458a2880d
Kasope Johnson <kasopej@gmail.com>
2021-09-24 11:18:17 +0100

    Made more responsive rev1

--- src/App.vue
+++ src/App.vue
@@ -33,7 +33,7 @@ body{padding: 0; margin: 0;}
 nav{display: flex; justify-content: space-between; align-items: center; background-color: blue; margin-bottom: 0;}
 ul{list-style: none; padding: 0;} 
 h1{text-align: start; color: black; padding-left: 10px;}
-.hero-container{width: 100%; background-color: rgb(20, 17, 51);}
+.hero-container{width: 100%; background-color: rgb(20, 17, 51); padding: 20px;}
 .message{background-color: blue; padding: 10px; font-weight: bold; text-transform: capitalize;}
 .header-bar-links{margin: 0 10px;}
 .header-bar-links li{display: inline-block; padding: 0 5px;}
